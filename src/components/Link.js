c4cfdb93ac369fd365a7faf469c24267790028c8
pluwum <luwyxx@gmail.com>
2018-07-11 20:48:09 +0300

    Add Link component

---
+++ src/components/Link.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+class Link extends Component{
+    render(){
+        return (
+            <div>
+                <div>
+                    {this.props.link.description} ({this.props.link.url})
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Link;
