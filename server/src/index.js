dda33a8bba0f0c0d31241a15a63742a4e2aba435
pluwum <luwyxx@gmail.com>
2018-07-15 17:17:04 +0300

    Update prisma endpoint

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'https://eu1.prisma.sh/public-lilyarm-780/hackernews-graphql-js/dev',
+      endpoint: 'https://eu1.prisma.sh/public-nightspeaker-202/hackernews-graphql-js/dev',
       secret: 'mysecret123',
       debug: true
     }),
